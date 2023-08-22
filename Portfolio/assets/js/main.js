362399ad8822f17ce3fd3f66553e12b44e139fdd
BasemYahia402 <basemyahia70@gmail.com>
2023-08-22 23:42:15 +0300

    add Scrool with ofset on links to main.js

--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -111,5 +111,27 @@
     this.classList.toggle("bi-list");
     this.classList.toggle("bi-x");
   });
-  
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on(
+    "click",
+    ".scrollto",
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
+
+        let navbar = select("#navbar");
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
 })();
