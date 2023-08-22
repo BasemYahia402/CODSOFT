3dbc885090a6e408e5a6a57e86b6c42f3fcd9227
BasemYahia402 <basemyahia70@gmail.com>
2023-08-22 23:40:52 +0300

    add Mobile nav toggle to main.js

--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -102,4 +102,14 @@
     window.addEventListener("load", toggleBacktotop);
     onscroll(document, toggleBacktotop);
   }
+
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
+  
 })();
